0b4ab75b03ce53d760d9c116f5012f4877acb9a0
Alexander <alexanderbely98@gmail.com>
2019-06-24 03:27:21 +0300

    Update Story.java
    Code Review

--- MavenConsoleChat/server/src/main/java/org/maksim/chakur/server/Story.java
+++ MavenConsoleChat/server/src/main/java/org/maksim/chakur/server/Story.java
@@ -3,10 +3,11 @@ package org.maksim.chakur.server;
 import java.util.LinkedList;
 
 // Класс для хранения пропущенных от клиентов сообщений
-public class Story {
-    private LinkedList<String> store = new LinkedList<>();
+public class Story { //Wrong name of class. Must be named "MessageHistory".
+    private LinkedList<String> store = new LinkedList<>();//Wrong name of variable. Must be named like "messageList".
 
-    public void addStory(String msg) {
+    //Why do we keep only last 5 messages from user? Is it not better to keep them all?
+    public void addStory(String msg) {//Wrong method name. Must be named like "addMessageToHistory".
         if (store.size() >= 5) {
             store.removeFirst();
             store.add(msg);
@@ -15,8 +16,8 @@ public class Story {
         }
     }
 
-    public String printStory() {
-        StringBuffer stringBuffer = new StringBuffer();
+    public String printStory() {//Wrong method name because of wrong variable name.
+        StringBuffer stringBuffer = new StringBuffer();//Why do we using stringBuffer?
         for (int i = 0; i < store.size(); i++) {
             stringBuffer.append(store.get(i));
             if (i < store.size()-1) {
@@ -26,7 +27,7 @@ public class Story {
         return stringBuffer.toString();
     }
 
-    public LinkedList<String> getStore() {
+    public LinkedList<String> getStore() {//Wrong method name because of wrong variable name.
         return store;
     }
 }
