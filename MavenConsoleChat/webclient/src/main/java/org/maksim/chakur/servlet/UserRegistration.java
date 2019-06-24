2377daf0afee4ef4c4e6fd77d75734b3d4adb3ca
Alexander <alexanderbely98@gmail.com>
2019-06-24 03:32:30 +0300

    Update UserRegistration.java
    Code Review

--- MavenConsoleChat/webclient/src/main/java/org/maksim/chakur/servlet/UserRegistration.java
+++ MavenConsoleChat/webclient/src/main/java/org/maksim/chakur/servlet/UserRegistration.java
@@ -3,7 +3,7 @@ package org.maksim.chakur.servlet;
 
 import java.io.IOException;
 
-import javax.servlet.ServletContext;
+import javax.servlet.ServletContext; //Unused import.
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +14,7 @@ public class UserRegistration extends Dispatcher {
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Unused code must be deleted.
 		// ServletContext ctx = getServletContext();
 		if (request.getParameter("login") != null) {
 			 this.forward("/CheckUser", request, response);
