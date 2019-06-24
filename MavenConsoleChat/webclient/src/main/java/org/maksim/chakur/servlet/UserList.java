718a26f96b603592ac189637517a731b21d150d0
Alexander <alexanderbely98@gmail.com>
2019-06-24 03:31:47 +0300

    Update UserList.java
    Code Review

--- MavenConsoleChat/webclient/src/main/java/org/maksim/chakur/servlet/UserList.java
+++ MavenConsoleChat/webclient/src/main/java/org/maksim/chakur/servlet/UserList.java
@@ -5,13 +5,13 @@ import java.util.HashMap;
 public class UserList {
 	private static HashMap<String, User> users = new HashMap<>();
 	public static User findUser(String name){
-        return (User)users.get(name);
+        return (User)users.get(name);//Unnecessary casting to User.
     }
 
     public static boolean addUser(User user){
         boolean result = false;
         if ((!users.containsKey(user.getName())) && (!"".equals(user.getName()) && user.getName().matches("^\\S+\\s*\\S+$") &&
-                (user.getPassword()!= null) && (!"".equals(user.getPassword())) && (user.getCharacter()!= null))){
+                (user.getPassword()!= null) && (!"".equals(user.getPassword())) && (user.getCharacter()!= null))){//Too much conditions. Need to make a method, that checks them.
             users.put(user.getName(), user);
             result = true;
         }
