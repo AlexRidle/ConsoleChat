63ad414a5a8e4073f8a2b798b22621c78b60f855
Alexander <alexanderbely98@gmail.com>
2019-06-24 03:29:06 +0300

    Update AddUser.java
    Code Review

--- MavenConsoleChat/webclient/src/main/java/org/maksim/chakur/servlet/AddUser.java
+++ MavenConsoleChat/webclient/src/main/java/org/maksim/chakur/servlet/AddUser.java
@@ -9,24 +9,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/AddUser")
-public class AddUser extends Dispatcher {
+public class AddUser extends Dispatcher {//Wrong name of class. Should be a noun, not a verb.
 	private static final long serialVersionUID = 1L;
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		ServletContext ctx = getServletContext();
-		
+
+		//Suspicious "if-elseif" construction.
 		if (request.getParameter("save") != null) {
+			//unnecessary values, they need only for creating a new object. Can be simplified.
 			String name = request.getParameter("name");
             String password = request.getParameter("password");
             String character = request.getParameter("character");
-            
+
             User newUser = new User(name, password, character);
 
             ctx.setAttribute("user", newUser);
 
+            //Unnecessary boolean. Easier to place it right in condition.
             boolean res = UserList.addUser(newUser);
-            
+
             if (res) {
             	this.forward("/SuccessRegistration.jsp", request, response);
             } else {
